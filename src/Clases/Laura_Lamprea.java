87f41539aa8ec914cb0fa1e7e7dfb394b070b271
Laura Lamprea <laura.lamprea@uniminuto.edu.co>
2023-02-13 20:20:59 -0500

    Presentacion

---
+++ src/Clases/Laura_Lamprea.java
@@ -0,0 +1,16 @@
+package Clases;
+
+public class Laura_Lamprea {
+    public static void main(String[] args) {
+
+        System.out.println(657301);
+        System.out.println("Laura Natalia Lamprea Rodriguez");
+        System.out.println(10);
+        System.out.println("pintar, dibujar");
+        System.out.println("Escribir");
+        System.out.println("Gato");
+
+
+    }
+
+}
