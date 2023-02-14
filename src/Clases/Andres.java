98bb8371e3ae8315843bebf677337819c05e5e6b
zip-b105-e-009 <zip-b105-e-009@UMD.LOCAL.EDU>
2023-02-13 20:49:38 -0500

    Informacion personal

--- src/Clases/Andres.java
+++ src/Clases/Andres.java
@@ -1,4 +1,16 @@
 package Clases;
 
 public class Andres {
+    public static void main(String[] args) {
+
+        int id = 708115;
+        String nombre = "Yerson Andres Martinez Lopez", semestre = "Noveno Semestre", hobbie = "Ver series, anime, escuchar musica, jugar videojuegos", animal = "Lobo, Aguila, Elefante";
+
+        System.out.println(id);
+        System.out.println(nombre);
+        System.out.println(semestre);
+        System.out.println(hobbie);
+        System.out.println(animal);
+
+    }
 }
