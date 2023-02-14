3f08d199469efcfe19a48f0ad36002e19ff85af2
Esteban Ruiz Novoa <esteban.ruiz@uniminuto.edu.co>
2023-02-13 20:40:54 -0500

    Commit clase esteban

---
+++ src/Clases/Esteban_Ruiz.java
@@ -0,0 +1,17 @@
+package Clases;
+
+public class Esteban_Ruiz {
+
+    public static void main(String[] args) {
+        int id = 703600;
+
+        String nombre = "Esteban Andres Ruiz Novoa";
+
+        int semestre = 9;
+
+        String hobby = "Jugar y ver futbol";
+
+        String animal = "gato";
+    }
+
+}
