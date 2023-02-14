0eae2731ff52d51ab4af771b5f4d3e782ca71e29
jsbecerra <jhon.becerra@uniminuto.edu.co>
2023-02-13 20:21:47 -0500

    informacion john

---
+++ src/Clases/John_Becerra.java
@@ -0,0 +1,21 @@
+package Clases;
+
+public class John_Becerra {
+
+    public static void main(String[] args) {
+        String nombre="John Becerra";
+        int semestre=9;
+        String hobby="Me gustan los videojuegos";
+        String disgustos="El egoismo";
+        String animal="Tiburon";
+
+
+        System.out.println("nombre:"+nombre);
+        System.out.println("Semestre:"+semestre+"no");
+        System.out.println("hobby:"+hobby);
+        System.out.println("Disgusto:"+disgustos);
+        System.out.println("Animal preferido:"+animal);
+    }
+}
+
+
